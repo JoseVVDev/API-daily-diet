a5a964d6423feb6ef3739b0d60e0fff916a24e21
JoseVVDev <78755947+JoseVVDev@users.noreply.github.com>
2023-06-30 11:16:08 -0300

    enviroment variables and user column in meals table

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,13 +1,6 @@
-import { FastifyInstance, FastifyRequest } from 'fastify'
+import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
-
-type CustomRequest = FastifyRequest<{
-  Body: {
-    name: string
-    description: string
-    diet: boolean
-  }
-}>
+import { z } from 'zod'
 
 export async function mealsRoutes(app: FastifyInstance) {
   app.get('/', async () => {
@@ -15,8 +8,15 @@ export async function mealsRoutes(app: FastifyInstance) {
     return tables
   })
 
-  app.post('/', async (request: CustomRequest, reply) => {
-    const { name, description, diet } = request.body
+  app.post('/', async (request, reply) => {
+    const requestBodySchema = z.object({
+      name: z.string(),
+      description: z.string(),
+      diet: z.boolean(),
+    })
+
+    const { name, description, diet } = requestBodySchema.parse(request.body)
+
     await knex('meals').insert({
       name,
       description,
